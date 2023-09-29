6a015c2f7f2be600742f3a1e4a38e746194582b7
frexxx-7 <borzenkokiril9@gmail.com>
2023-09-29 15:30:18 +0300

    finished registration and edit main page

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import classes from './Main.module.scss'
+import axios from 'axios'
 
 const Main = () => {
+
+  const [userInfo, setUserInfo] = useState([])
+
+  useEffect(() => {
+    getUserInfo()
+  }, [])
+
+  const getUserInfo = () => {
+    axios.get("http://hotel.com").then(response => {
+      setUserInfo(response.data)
+    })
+  }
+
   return (
-    <div>Main</div>
+    <div>
+      {userInfo.map((user, key) =>
+        <div key={key}>
+          <p>{user.id}</p>
+          <p>{user.login}</p>
+          <p>{user.password}</p>
+        </div>
+      )}
+    </div>
   )
 }
 
--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 import classes from './Registration.module.scss'
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Registration = () => {
     const navigate = useNavigate();
 
     const [user, setUser] = useState({})
-
-    const [login, setLogin] = useState();
-    const [password, setPassword] = useState();
+    const [repeatPassword, setRepeatPassword] = useState()
+    const [error, setError] = useState("")
 
     const changeUser = (e) => {
         const name = e.target.name
@@ -16,23 +16,40 @@ const Registration = () => {
         setUser(values => ({ ...values, [name]: value }))
     }
 
-    async function postData(url = "", data = {}) {
-        // Default options are marked with *
-        const response = await fetch(url, {
-            method: "POST", // *GET, POST, PUT, DELETE, etc.
-            mode: "cors", // no-cors, *cors, same-origin
-            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: "same-origin", // include, *same-origin, omit
-            headers: {
-                // "Content-Type": "application/json",
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            redirect: "follow", // manual, *follow, error
-            referrerPolicy: "no-referrer", // no-referrer, *client
-            body: JSON.stringify(data), // body data type must match "Content-Type" header
-        });
-        return await response
-    }
+    // async function postData(url = "", data = {}) {
+    //     // Default options are marked with *
+    //     const response = await fetch(url, {
+    //         method: "POST", // *GET, POST, PUT, DELETE, etc.
+    //         mode: "cors", // no-cors, *cors, same-origin
+    //         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    //         credentials: "same-origin", // include, *same-origin, omit
+    //         headers: {
+    //             // "Content-Type": "application/json",
+    //             'Content-Type': 'application/x-www-form-urlencoded',
+    //         },
+    //         redirect: "follow", // manual, *follow, error
+    //         referrerPolicy: "no-referrer", // no-referrer, *client
+    //         body: JSON.stringify(data), // body data type must match "Content-Type" header
+    //     });
+    //     return await response
+    // }
+    // async function getData(url = "", data = {}) {
+    //     // Default options are marked with *
+    //     const response = await fetch(url, {
+    //         method: "POST", // *GET, POST, PUT, DELETE, etc.
+    //         mode: "cors", // no-cors, *cors, same-origin
+    //         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    //         credentials: "same-origin", // include, *same-origin, omit
+    //         headers: {
+    //             // "Content-Type": "application/json",
+    //             'Content-Type': 'application/x-www-form-urlencoded',
+    //         },
+    //         redirect: "follow", // manual, *follow, error
+    //         referrerPolicy: "no-referrer", // no-referrer, *client
+    //         body: JSON.stringify(data), // body data type must match "Content-Type" header
+    //     });
+    //     return await response
+    // }
 
     const registrationClick = (e) => {
 
@@ -47,11 +64,30 @@ const Registration = () => {
         // .then(response => {
         //     console.log(response)
         // })
-        postData("http://hotel.com", { ...user }).then((response) => {
-            console.log(response.data);
-        })
+        // postData("http://hotel.com", { ...user }).then((response) => {
+        //     console.log(response.data);
+        // })
+        if (!user.login)
+            setError("Введите логин")
+        else
+            if (!user.password)
+                setError("Введите пароль")
+            else
+                if (!repeatPassword)
+                    setError("Повторите пароль")
+                else
+                    if (user.password != repeatPassword)
+                        setError("Пароли не совпадают")
+                    else
+                        if (user.password.length < 8)
+                            setError("Длина пароля должна быть больше 8")
+                        else
+                            axios.post("http://hotel.com", user).then(response => {
+                                if (response.data.status == 1) {
+                                    navigate('/main')
+                                }
+                            })
 
-        // navigate('/main')
     }
 
     return (
@@ -60,10 +96,11 @@ const Registration = () => {
                 <div className={classes.windowContent}>
                     <p>Регистрация</p>
                     <div className={classes.inputDiv}>
-                        <input type="text" id="loginInput" className={classes.inputRegistration} name="login" placeholder='Логин' onChange={changeUser} />
-                        <input type="text" id="passwordInput" className={classes.inputRegistration} name="password" placeholder='Пароль' onChange={changeUser} />
-                        <input type="text" className={classes.inputRegistration} placeholder='Повторите пароль' />
+                        <input type="text" className={classes.inputRegistration} name="login" placeholder='Логин' onChange={changeUser} />
+                        <input type="text" className={classes.inputRegistration} name="password" placeholder='Пароль' onChange={changeUser} />
+                        <input type="text" className={classes.inputRegistration} placeholder='Повторите пароль' onChange={(e) => setRepeatPassword(e.target.value)} />
                     </div>
+                    <p className={classes.error}>{error}</p>
                     <button className={classes.registrationButton} onClick={registrationClick}>Зарегистрироваться</button>
                 </div>
             </div>
