f0adaae920a55151e4058c6a2f501f13a30176b5
frexxx-7 <borzenkokiril9@gmail.com>
2023-10-06 08:18:08 +0300

    add signin

--- src/pages/Autorization/Autorization.jsx
+++ src/pages/Autorization/Autorization.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import classes from './Autorization.module.scss'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useStateContext } from '../../context/ContextProvider'
+import axiosCLient from '../../axios.client'
 
 const Autorization = () => {
+    const navigate = useNavigate()
+
+    const emailRef = useRef()
+    const passwordRef = useRef()
+
+    const [errors, setErrors] = useState()
+    const { setUser, setToken } = useStateContext()
+
+    const autorizationCLick = () => {
+        const payload = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value
+        }
+
+        axiosCLient.post('/signin', payload)
+            .then(({ data }) => {
+                setUser(data.user)
+                setToken(data.token)
+
+            })
+            .catch(err => {
+                const response = err.response
+                if (response && response.status === 422) {
+                    setErrors(response.data.errors)
+                }
+            })
+    }
+
     return (
         <div className={classes.mainContent}>
             <div className={classes.company}>
@@ -14,17 +44,22 @@ const Autorization = () => {
                     <div className={classes.windowContent}>
                         <p>Авторизация</p>
                         <div className={classes.inputDiv}>
-                            <input type="text" className={classes.inputAutorization} placeholder='Login' />
-                            <input type="text" className={classes.inputAutorization} placeholder='Password' />
+                            <input ref={emailRef} type="text" className={classes.inputAutorization} placeholder='Почта' />
+                            <input ref={passwordRef} type="text" className={classes.inputAutorization} placeholder='Пароль' />
                         </div>
-                        {/* <div style={{ color: 'red', textAlign: 'center', marginTop: '1rem' }}></div> */}
+                        {errors &&
+                            <div>
+                                {Object.keys(errors).map(key => (
+                                    <p className={classes.error} key={key}>{errors[key][0]}</p>
+                                ))}</div>
+                        }
                         <Link
                             to="/signup"
                             className={classes.registrationA}
                         >
                             Регистрация
                         </Link>
-                        <button className={classes.autorizationButton}>Авторизация</button>
+                        <button className={classes.autorizationButton} onClick={autorizationCLick}>Авторизация</button>
                     </div>
                 </div>
 
